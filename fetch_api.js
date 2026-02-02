96c2ac217add450d7bacf0cccdfd3f5f47f2b9c5
Qasim3799 <qasim.adebisik@gmail.com>
2026-02-02 03:50:49 -0800

    Update fetch_api.js
    add dog breed to the output. Extracted the dog breed from the api message and displayed it below the image

--- fetch_api.js
+++ fetch_api.js
@@ -6,9 +6,34 @@ const apiUrl = 'https://dog.ceo/api/breeds/image/random'
 
 
 // function to fetch a random dog image from the api url
-const getImage = () => {
-    fetch(apiUrl).then(res => res.json()).then(data => {
-            dogImageElement.src = data.message;
+const getImage = async () => {
+    const res = await fetch(apiUrl);
+
+    try{
+        if(!res.ok){
+            alert('Something went wrong')
+            throw new Error('Something went wrong')
         }
-    )
+
+        const data = await res .json()
+        console.log(data)
+        // dogImageElement.src = data.message
+
+        const url = data.message
+        const pattern = /breeds\/\w*-?\w*/gi    // regex pattern to extract the dog breed from the url
+        const breed = url.match(pattern)[0].replace('breeds/', '')
+
+        display.innerHTML = `
+            <img id="dog-image" src="${data.message}">
+            <p id='breed'>${breed}</p>
+        `
+    }
+    catch(err){
+        console.error(err)
+    }
 }
+
+
+// async function getImage(){
+
+// }
